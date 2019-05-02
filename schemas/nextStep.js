de8a676cf409d5e8773d7cf9b7c63e93942f3c79
tommyno <tommy@spindelvev.net>
2019-05-02 12:51:52 +0200

    Move fields into Step

--- schemas/nextStep.js
+++ schemas/nextStep.js
@@ -48,6 +48,77 @@ export default {
               title: 'Steg beskrivelse',
               type: 'text',
             },
+
+            // CORE PROCESS
+            {
+              name: 'coreProcesses',
+              title: 'Kjerneprosesser',
+              type: 'array',
+              of: [{ type: 'coreProcess' }],
+            },
+
+            // MANAGEMENT PROCESS
+            {
+              name: 'managementProcess',
+              title: 'Ledelsesprosesser',
+              type: 'array',
+              of: [
+                {
+                  name: 'process',
+                  title: 'Prosess',
+                  type: 'object',
+                  fields: [
+                    {
+                      name: 'title',
+                      title: 'Tittel',
+                      type: 'string',
+                    },
+                    {
+                      name: 'process',
+                      title: 'Prosesser',
+                      type: 'array',
+                      of: [
+                        {
+                          name: 'processes',
+                          title: 'Prosesser',
+                          type: 'object',
+                          fields: [
+                            {
+                              name: 'processItem',
+                              title: 'Prosesspunkt',
+                              type: 'string',
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                    {
+                      name: 'sustainabilityCheck',
+                      title: 'Bærekraft',
+                      type: 'string',
+                      options: {
+                        list: [{ title: 'Økonomi', value: 'economics' }, { title: 'Miljø', value: 'enviroment' }, { title: 'Sosialt', value: 'social' }],
+                        layout: 'dropdown',
+                      },
+                    },
+                  ],
+                  preview: {
+                    select: {
+                      title: 'title',
+                      subtitle: 'sustainabilityCheck',
+                    },
+                  },
+                },
+              ],
+            },
+
+            // DELIVERY
+            {
+              name: 'deliveryFail',
+              title: 'Feil ved overlevering?',
+              description: 'Gå tilbake til steg 1 ved feil',
+              type: 'boolean',
+            },
           ],
           preview: {
             select: {
@@ -64,77 +135,6 @@ export default {
           },
         },
       ],
-    },
-
-    // CORE PROCESS
-    {
-      name: 'coreProcesses',
-      title: 'Kjerneprosesser',
-      type: 'array',
-      of: [{ type: 'coreProcess' }],
-    },
-
-    // MANAGEMENT PROCESS
-    {
-      name: 'managementProcess',
-      title: 'Ledelsesprosesser',
-      type: 'array',
-      of: [
-        {
-          name: 'process',
-          title: 'Prosess',
-          type: 'object',
-          fields: [
-            {
-              name: 'title',
-              title: 'Tittel',
-              type: 'string',
-            },
-            {
-              name: 'process',
-              title: 'Prosesser',
-              type: 'array',
-              of: [
-                {
-                  name: 'processes',
-                  title: 'Prosesser',
-                  type: 'object',
-                  fields: [
-                    {
-                      name: 'processItem',
-                      title: 'Prosesspunkt',
-                      type: 'string',
-                    },
-                  ],
-                },
-              ],
-            },
-            {
-              name: 'sustainabilityCheck',
-              title: 'Bærekraft',
-              type: 'string',
-              options: {
-                list: [{ title: 'Økonomi', value: 'economics' }, { title: 'Miljø', value: 'enviroment' }, { title: 'Sosialt', value: 'social' }],
-                layout: 'dropdown',
-              },
-            },
-          ],
-          preview: {
-            select: {
-              title: 'title',
-              subtitle: 'sustainabilityCheck',
-            },
-          },
-        },
-      ],
-    },
-
-    // DELIVERY
-    {
-      name: 'deliveryFail',
-      title: 'Feil ved overlevering?',
-      description: 'Gå tilbake til steg 1 ved feil',
-      type: 'boolean',
     },
   ],
   preview: {
