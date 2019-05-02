c658d6fe8a1bb1cccea66d197a5420dd7a447dab
tommyno <tommy@spindelvev.net>
2019-05-02 11:19:52 +0200

    Remove movie dummy schemas

--- schemas/schema.js
+++ schemas/schema.js
@@ -5,16 +5,6 @@ import schemaTypes from 'all:part:@sanity/base/schema-type';
 
 // We import object and document schemas
 import nextStep from './nextStep';
-
-// TODO: Remove dummy data
-import blockContent from './blockContent';
-import crewMember from './crewMember';
-import castMember from './castMember';
-import movie from './movie';
-import person from './person';
-import screening from './screening';
-import plotSummary from './plotSummary';
-import plotSummaries from './plotSummaries';
 import coreProcess from './coreProcess';
 
 // Then we give our schema to the builder and provide the result to Sanity
@@ -26,17 +16,9 @@ export default createSchema({
   types: schemaTypes.concat([
     // The following are document types which will appear
     // in the studio.
-    movie,
-    person,
-    screening,
     nextStep,
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
-    blockContent,
-    plotSummary,
-    plotSummaries,
-    castMember,
-    crewMember,
     coreProcess,
   ]),
 });
